6199aba992f253dd0dac3bf8267de93d526a6198
rsenthamil <31533424+rsenthamil@users.noreply.github.com>
2017-12-11 11:52:50 +0530

    Update BlogDAO.java

--- SocialNetworkBackEnd/SocialNetworkBackEnd/src/main/java/com/SocialNetworkBackEnd/Dao/BlogDAO.java
+++ SocialNetworkBackEnd/SocialNetworkBackEnd/src/main/java/com/SocialNetworkBackEnd/Dao/BlogDAO.java
@@ -1,15 +1,19 @@
 package com.SocialNetworkBackEnd.Dao;
+
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.SocialNetworkBackEnd.Model.Blog;
-public interface BlogDAO {
+
+@Service
+public interface BlogDao {
+
     public boolean addBlog(Blog blog);
-	public boolean updateBlog(Blog blog);
-	public boolean deleteBlog(Blog blog);
-	public Blog getBlog(int blogId);
-	public List <Blog> getAllBlogs();
-	public boolean approveBlog(Blog blog);
-
-	}
-
-
+	public boolean updateblog(Blog blog);
+	public boolean deleteblog(Blog blog);
+	public Blog getblogbyid(int blogId);
+	public List<Blog> getallblogs();
+	public boolean approveblog(Blog blog);
+	public boolean rejectblog(Blog b);
+}
