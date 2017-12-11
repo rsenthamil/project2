3e2560d48bf9abc46ea373e64fe9c68c8617a2b2
rsenthamil <31533424+rsenthamil@users.noreply.github.com>
2017-12-11 12:01:10 +0530

    Update UserDaoImpl.java

--- SocialNetworkBackEnd/SocialNetworkBackEnd/src/main/java/com/SocialNetworkBackEnd/Dao/UserDaoImpl.java
+++ SocialNetworkBackEnd/SocialNetworkBackEnd/src/main/java/com/SocialNetworkBackEnd/Dao/UserDaoImpl.java
@@ -1,18 +1,23 @@
 package com.SocialNetworkBackEnd.Dao;
 
+import java.util.List;
+
+//import javax.persistence.Query;
 import javax.transaction.Transactional;
 
 import org.hibernate.Session;
 //import org.hibernate.Query;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import com.SocialNetworkBackEnd.Model.UserDetails;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import com.SocialNetworkBackEnd.Model.UserDetail;
 
 @Repository
-
+@Transactional
 public class UserDaoImpl  implements UserDao{
     
 	@Autowired
@@ -27,7 +32,7 @@ public class UserDaoImpl  implements UserDao{
 	 */
 	@Transactional
 //	@Override
-	public boolean addUser(UserDetails user) {
+	public boolean addUser(UserDetail user) {
 		try
 		{
 			Session session=sessionfactory.openSession();
@@ -49,15 +54,104 @@ public class UserDaoImpl  implements UserDao{
 		return false;
 	}
    
-	//@Override
-	public boolean updateOnlineStatus(String status, UserDetails user) {
-			return false;
+ @Transactional
+public boolean updateuser(UserDetail user)
+{
+try
+{
+Session session=sessionfactory.openSession();
+Transaction transaction=session.getTransaction();
+transaction.begin();
+session.saveOrUpdate(user);
+session.flush();
+transaction.commit();
+session.close();
+System.out.println("updated successfully");
+return true;
+}
+catch(Exception e)
+{
+System.out.println(e);
+return false;    
+}
+}
+@Transactional
+public boolean deleteuser(UserDetail user)
+{
+try
+{
+	Session session=sessionfactory.openSession();
+	Transaction transaction=session.getTransaction();
+	transaction.begin();
+session.delete(user);
+transaction.commit();
+session.close();
+System.out.println("deleted successfully");
+return true;
+}
+catch(Exception e)
+{
+System.out.println(e);
+return false;    
+}
+}
+
+@Transactional
+public boolean updateOnlineStatus(UserDetail user)
+{
+try
+{
+user.setIsOnline(user.getIsOnline());
+Session session=sessionfactory.openSession();
+Transaction transaction=session.getTransaction();
+transaction.begin();
+session.saveOrUpdate(user);
+transaction.commit();
+session.close();
+System.out.println("online status updated");
+return true;
+}
+catch(Exception e)
+{
+   System.out.println(e);
+   return false;    
+}
+}	
+
+	public List<UserDetail> getalluser() {
+		// TODO Auto-generated method stub
+		Session session=sessionfactory.openSession();
+		Transaction transaction=session.getTransaction();
+		transaction.begin();
+		Query query=session.createQuery("from UserDetail");
+		//System.out.println("****"+query);
+		List<UserDetail> userdetail=query.list();
+		
+		//List<UserDetail> userdetail=session.createQuery("from UserDetail").list();
+	//	System.out.println("@@:"+userdetail);
+	    
+		//session.close();
+	    return userdetail;
 	}
 
-	public Object getAllUser() {
-		// TODO Auto-generated method stub
-		return null;
+	    public UserDetail getuserbyid(int userId) 
+	    {
+	        
+	        Session session=sessionfactory.openSession();
+	        Transaction transaction=session.getTransaction();
+	        transaction.begin();
+	        UserDetail ud=session.get(UserDetail.class,new Integer(userId));
+	        session.flush();
+	        transaction.commit();
+	        session.close();
+	        return ud;
+	         
+	    }
+
 	}
 
 	
-}
+
+
+	
+
