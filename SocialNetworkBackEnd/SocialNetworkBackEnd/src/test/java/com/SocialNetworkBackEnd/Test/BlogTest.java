874e7cbf00d715a13809fbcc6bf4b1bad5286ed4
rsenthamil <31533424+rsenthamil@users.noreply.github.com>
2017-12-11 12:09:26 +0530

    Update BlogTest.java

--- SocialNetworkBackEnd/SocialNetworkBackEnd/src/test/java/com/SocialNetworkBackEnd/Test/BlogTest.java
+++ SocialNetworkBackEnd/SocialNetworkBackEnd/src/test/java/com/SocialNetworkBackEnd/Test/BlogTest.java
@@ -1,44 +1,117 @@
-package com.SocialNetworkBackEnd.Test;
+package SocialNetworkRest.SocialNetworkBackEnd.test;
+
 import static org.junit.Assert.*;
 
+import java.util.Date;
+import java.util.List;
 
+import javax.transaction.Transactional;
 
-import org.junit.BeforeClass;
+import org.hibernate.SessionFactory;
+import org.junit.Ignore;
 import org.junit.Test;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
-import com.SocialNetworkBackEnd.Dao.BlogDAO;
+import com.SocialNetworkBackEnd.Config.ApplicationContextConfig;
+import com.SocialNetworkBackEnd.Dao.BlogDao;
+//import com.SocialNetworkBackEnd.Dao.UserDao;
 import com.SocialNetworkBackEnd.Model.Blog;
+//import com.SocialNetworkBackEnd.Model.UserDetail;
 
-public class BlogTest 
-{
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes={ApplicationContextConfig.class})
+@WebAppConfiguration
+@Transactional
 
-	static BlogDAO blogDAO;
-	
-	@BeforeClass
-	public static void initialize()
-	{
-		AnnotationConfigApplicationContext context=new AnnotationConfigApplicationContext();
-		context.scan("com.SocialNetworkBackEnd.*");
-		context.refresh();
+public class BlogDaoTest {
+
+	 @Autowired
+	  BlogDao blogdao;
+		@Autowired
+	    public Blog blog;
 		
-		blogDAO=(BlogDAO)context.getBean("blogDAO");
-	}
-	
-	@Test
-	public void addBlogTest()
-	{
-		Blog blog=new Blog();
+		@Autowired
+		SessionFactory sessionFactory;
 		
-		blog.setBlogId(1001);
-		blog.setBlogName("Core Java");
-		blog.setBlogContent("It is based on Simple Java Concept");
-		blog.setUsername("sunil");
-		blog.setStatus("A");
-		blog.setLikes(3);
-		blog.setCreateDate(new java.util.Date());
 		
-		assertTrue("Problem in Inserting Blog",blogDAO.addBlog(blog));
-	
-	}
+		//@Ignore
+		@Test
+		public void addBlogTest()
+		{
+			Blog blog=new Blog();
+		//	blog.setBlogId(1001);
+			blog.setBlogName("Core Java");
+			blog.setBlogContent("It is based on Simple Java Concept");
+			blog.setUserid(2);
+			blog.setStatus("A");
+			blog.setLikes(3);
+			blog.setCreateDate(new java.util.Date());
+			System.out.println("Check");
+			assertTrue("Problem in Inserting Blog",blogdao.addBlog(blog));
+			System.out.println("Check2");
+
+		}
+		
+		@Ignore
+		@Test
+		public void updateblogTest()
+		{
+			Blog b=blogdao.getblogbyid(4);
+			b.setBlogName("About angular framework");
+			b.setBlogContent("explaining angular concept");
+			b.setStatus("A");
+			b.setLikes(500);
+			//b.setUserId(21);
+			b.setCreateDate(new Date());
+			assertTrue("Problem in updating blog",blogdao.updateblog(b));
+		}
+		@Ignore
+		@Test
+		public void getblogbyidTest()
+		{
+			Blog b=blogdao.getblogbyid(4);
+			System.out.println("getblogbyid() method invoked");
+			System.out.println("blogstatus = "+b.getStatus());
+		}
+		@Ignore
+		@Test
+		public void deleteblogTest()
+		{
+			Blog b=blogdao.getblogbyid(5);
+			assertTrue("problem in deleting blog",blogdao.deleteblog(b));
+		}
+		@Ignore
+		@Test
+		public void getallblogTest()
+		{
+			List<Blog> l=blogdao.getallblogs();
+			for(Blog b:l)
+			{
+				System.out.println(b.getBlogId());
+			}
+		}
+		@Ignore
+		@Test
+		public void approveblogTest()
+		{
+		Blog b=blogdao.getblogbyid(26);
+		System.out.println("status before change"+b.getStatus());
+		b.setStatus("A");
+		assertTrue("problem in approving blog id",blogdao.approveblog(b));
+		}
+		@Ignore
+		@Test
+		public void rejectblogTest()
+		{
+			Blog b=blogdao.getblogbyid(27);
+			b.setStatus("N");
+			assertTrue("problem in rejecting  blog id",blogdao.rejectblog(b));	
+		}
+
+		
+
 }
